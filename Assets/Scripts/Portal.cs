fcab08ce90eed0d6a7ebf46e1c1be92bcb791101
maveric94 <maveric94@mail.ru>
2016-11-09 21:22:00 +0300

    portal added

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour, ICollisionReceiver
+{
+    [SerializeField] Transform exit;
+
+    public void CollisionEnter(Collision collisionInfo, int monitorID)
+    {
+        Character character = collisionInfo.gameObject.GetComponentInParent<Character>();
+        if (character != null)
+        {
+            character.MoveBy(exit.position - character.GetPosition());
+        }
+    }
+
+
+    public void CollisionExit(Collision collisionInfo, int monitorID)
+    {
+        
+    }
+}
